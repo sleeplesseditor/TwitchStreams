59601764603b638bf8f3e4cece76f4599a807ecf
sleeplesseditor <josh@bryson.me.uk>
2019-01-19 16:02:27 +0000

    StreamCreate form styling

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-    renderInput(formProps) {
+    renderInput({ input, label }) {
         return (
-            <input 
-                onChange={formProps.input.onChange}
-                value={formProps.input.value}
-            />
-        )
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
     }
 
     render() {
         return (
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
             </form>
         )
     }
