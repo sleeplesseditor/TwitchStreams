8791f4ee73a4e5742189ce25a845ef16e659ccde
sleeplesseditor <josh@bryson.me.uk>
2019-01-29 23:45:48 +0000

    Delete Modal converted to dynamic component

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const Modal = props => {
     return ReactDOM.createPortal(
         <div 
-            onClick={() => history.push("/")} 
+            onClick={props.onDismiss} 
             className="ui dimmer modals visible active"
         >
             <div 
                 onClick={(e) => e.stopPropagation()}
                 className="ui standard modal visible active"
             >
-                <div className="header">Delete Stream</div>
-                <div className="content">Are You Sure You Want to Delete this Stream?</div>
-                <div className="actions">
-                    <button className="ui button primary">Delete</button>
-                    <button className="ui button">Cancel</button>
-                </div>
+                <div className="header">{props.title}</div>
+                <div className="content">{props.content}</div>
+                <div className="actions">{props.actions}</div>
             </div>
         </div>,
         document.querySelector('#modal')
